2616c76956838e324ef0e9515f60d3c0b406ce21
JacopoMarini <marinijacopo96@gmail.com>
2022-11-25 22:23:03 +0100

    Add row in Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,12 +5,48 @@ function Board() {
   return (
     const [board, setBoard] = useState(baseBoard);
     return <div className='board'>
-     <div className='row'> <Letter={} attempt={} /> </div>
-     <div className='row'> <Letter={} attempt={} /> </div>
-     <div className='row'> <Letter={} attempt={} /> </div>
-     <div className='row'> <Letter={} attempt={} /> </div>
-     <div className='row'> <Letter={} attempt={} /> </div>
-     <div className='row'> <Letter={} attempt={} /> </div>
+
+      {/* Creo cinque righe che contengono ognuna ogni lettera, alla lettera passo la posizione e il tentativo */}
+     <div className='row'>
+       <Letter letterPos={0} attempt={0} />
+       <Letter letterPos={1} attempt={0} />
+       <Letter letterPos={2} attempt={0} />
+       <Letter letterPos={3} attempt={0} />
+       <Letter letterPos={4} attempt={0} /> 
+      </div>
+      {/* Incremento il tentativo, perchè se sono alla seconda riga sono al secondo tentativo */}
+      <div className='row'>
+       <Letter letterPos={0} attempt={1} />
+       <Letter letterPos={1} attempt={1} />
+       <Letter letterPos={2} attempt={1} />
+       <Letter letterPos={3} attempt={1} />
+       <Letter letterPos={4} attempt={1} /> 
+      </div>
+
+      <div className='row'>
+       <Letter letterPos={0} attempt={2} />
+       <Letter letterPos={1} attempt={2} />
+       <Letter letterPos={2} attempt={2} />
+       <Letter letterPos={3} attempt={2} />
+       <Letter letterPos={4} attempt={2} /> 
+      </div>
+
+      <div className='row'>
+       <Letter letterPos={0} attempt={3} />
+       <Letter letterPos={1} attempt={3} />
+       <Letter letterPos={2} attempt={3} />
+       <Letter letterPos={3} attempt={3} />
+       <Letter letterPos={4} attempt={3} /> 
+      </div>
+
+      <div className='row'>
+       <Letter letterPos={0} attempt={4} />
+       <Letter letterPos={1} attempt={4} />
+       <Letter letterPos={2} attempt={4} />
+       <Letter letterPos={3} attempt={4} />
+       <Letter letterPos={4} attempt={4} /> 
+      </div>
+     
     </div>
   )
 }
