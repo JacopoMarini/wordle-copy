e0d7e72013e7d0b881927f564ed4dca2e1e0dd05
JacopoMarini <marinijacopo96@gmail.com>
2022-11-25 22:17:26 +0100

    Second commit, scaffolding

---
+++ src/components/Board.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+import { baseBoard } from '../Words'
+
+function Board() {
+  return (
+    const [board, setBoard] = useState(baseBoard);
+    return <div className='board'>
+     <div className='row'> <Letter={} attempt={} /> </div>
+     <div className='row'> <Letter={} attempt={} /> </div>
+     <div className='row'> <Letter={} attempt={} /> </div>
+     <div className='row'> <Letter={} attempt={} /> </div>
+     <div className='row'> <Letter={} attempt={} /> </div>
+     <div className='row'> <Letter={} attempt={} /> </div>
+    </div>
+  )
+}
+
+export default Board
