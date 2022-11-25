1175bc1eaa9bb65a771dbce85e2f06cc2091b9fc
JacopoMarini <marinijacopo96@gmail.com>
2022-11-25 22:17:01 +0100

    First commit, add components

---
+++ src/Words.js
@@ -0,0 +1,10 @@
+// Creo un array di sei array (6 sono i tentativi massimi), ogni array ha 5 spazi per le lettere possibili da inserire all'interno 
+
+export const baseBoard = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
---
+++ src/components/Letter.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Letter({letterPosition, attempt}) {
+  return <div>Letter</div>
+}
+
+export default Letter
